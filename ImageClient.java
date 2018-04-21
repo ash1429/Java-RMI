89aba269f51382d08b6daa25e4b0356147f0513c
notash1429 <xamananik@gmail.com>
2018-04-22 02:39:59 +0600

    I guess manipulating 4 chunks of image sent from client in server, and then merging them back in client is done for now, roughly or so.....

--- ImageClient.java
+++ ImageClient.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 
 import java.awt.image.BufferedImage;
 import java.io.*;
+import java.awt.Graphics2D;
+import java.awt.Color;
+
 
 import javax.imageio.ImageIO;
 
@@ -97,23 +100,75 @@ public class ImageClient{
 			System.out.println(returnedImageC.getHeight());
 			System.out.println(returnedImageD.getHeight());
 
-	//writing output of each server
-			File outPutFileA = new File("returnedFromServerA.png");
-			ImageIO.write(returnedImageA, "png", outPutFileA);
+	//writing output from each server
 
-			File outPutFileB = new File("returnedFromServerB.png");
-			ImageIO.write(returnedImageB, "png", outPutFileB);
+			ImageIO.write(returnedImageA, "png", new File("returnedFromServerA.png"));
 
+			ImageIO.write(returnedImageB, "png", new File("returnedFromServerB.png"));
 
-			File outPutFileC = new File("returnedFromServerC.png");
-			ImageIO.write(returnedImageC, "png", outPutFileC);
+			ImageIO.write(returnedImageC, "png", new File("returnedFromServerC.png"));
 
-			File outPutFileD = new File("returnedFromServerD.png");
-			ImageIO.write(returnedImageD, "png", outPutFileD);
+			ImageIO.write(returnedImageD, "png", new File("returnedFromServerD.png"));
+
+	//merging output from each server
+		    BufferedImage joinedImgHorizontal1 = joinBufferedImageHorizontal(returnedImageA, returnedImageB);
+		    ImageIO.write(joinedImgHorizontal1, "png", new File("joinedImgHorizontal1.png"));
+
+		    BufferedImage joinedImgHorizontal2 = joinBufferedImageHorizontal(returnedImageC, returnedImageD);
+		    ImageIO.write(joinedImgHorizontal2, "png", new File("joinedImgHorizontal2.png"));
+
+		    BufferedImage joinedImg = joinBufferedImageVertical(joinedImgHorizontal1,joinedImgHorizontal2);
+		    ImageIO.write(joinedImg, "png", new File("Finaljoined.png"));
 
 
 		}catch(Exception e){
 			System.out.println("Exception in clinet: " + e);
 		}
 	}// main ends
-}
+
+	public static BufferedImage joinBufferedImageVertical(BufferedImage subImage1, BufferedImage subImage2) {
+	    int offset = 2;
+
+	    // int width = subImage1.getWidth() + subImage2.getWidth() + offset;
+
+	    int height = subImage1.getHeight() + subImage2.getHeight() + offset;
+
+	    // int height = Math.max(subImage1.getHeight(), subImage2.getHeight()) + offset;
+
+	    int width = Math.max(subImage1.getWidth(), subImage2.getWidth()) + offset;
+
+	    BufferedImage newImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+	    Graphics2D g2 = newImage.createGraphics();
+	    Color oldColor = g2.getColor();
+	    g2.setPaint(Color.BLACK);
+	    g2.fillRect(0, 0, width, height);
+	    g2.setColor(oldColor);
+	    g2.drawImage(subImage1, null, 0, 0);
+	    // g2.drawImage(subImage2, null, subImage1.getWidth() + offset, 0);
+	    g2.drawImage(subImage2, null, 0 , subImage1.getHeight() + offset);
+	    g2.dispose();
+	    return newImage;
+  	}
+
+  	public static BufferedImage joinBufferedImageHorizontal(BufferedImage subImage1, BufferedImage subImage2) {
+	    int offset = 2;
+
+	    int width = subImage1.getWidth() + subImage2.getWidth() + offset;
+
+	    int height = Math.max(subImage1.getHeight(), subImage2.getHeight()) + offset;
+
+	    BufferedImage newImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+	    Graphics2D g2 = newImage.createGraphics();
+	    Color oldColor = g2.getColor();
+	    g2.setPaint(Color.BLACK);
+	    g2.fillRect(0, 0, width, height);
+	    g2.setColor(oldColor);
+	    g2.drawImage(subImage1, null, 0, 0);
+	    g2.drawImage(subImage2, null, subImage1.getWidth() + offset, 0);
+	    g2.dispose();
+	    return newImage;
+  	}
+
+
+
+}//ImageClient ends
